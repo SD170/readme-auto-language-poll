3a09f96889e98282ad5762db2cf86faa9cd21aba
SD170 <duttasaswata7@gmail.com>
2022-07-21 20:51:09 +0530

    workflow test

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import path from 'path';
-// require('dotenv').config({ path: path.join(process.cwd()+'/.env') });
+require('dotenv').config({ path: path.join(process.cwd()+'/.env') });
 
 import { generateReadMe } from "./generator/markdown";
-
-//load env vars
-// dotenv.config({ path: './.env' });
 
 (async () => {
     await generateReadMe();
