3435f335ff069f9d4463548d4020cf47769b9874
SD170 <duttasaswata7@gmail.com>
2022-07-21 20:41:34 +0530

    workflow test

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import path from 'path';
 // require('dotenv').config({ path: path.join(process.cwd()+'/.env') });
 
-import { fetchDetails } from "./fetcher/github";
-import { fetchColors } from "./fetcher/githubColors";
 import { generateReadMe } from "./generator/markdown";
 
 //load env vars
 // dotenv.config({ path: './.env' });
 
 (async () => {
-    // const res = await fetchDetails();
-    // const res2 = await fetchColors();
     await generateReadMe();
-    // console.log(res);
-    // console.log(res2);
 })()
