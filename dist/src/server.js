94f176aafb5b1f74cc62893c90350fd093168992
SD170 <duttasaswata7@gmail.com>
2022-07-21 20:57:57 +0530

    workflow test

--- dist/src/server.js
+++ dist/src/server.js
@@ -8,11 +8,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// require('dotenv').config({ path: path.join(process.cwd()+'/.env') });
+const path_1 = __importDefault(require("path"));
+require('dotenv').config({ path: path_1.default.join(process.cwd() + '/.env') });
 const markdown_1 = require("./generator/markdown");
-//load env vars
-// dotenv.config({ path: './.env' });
 (() => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, markdown_1.generateReadMe)();
 }))();
